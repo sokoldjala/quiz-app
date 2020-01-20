0dd9743d31b99f65c2cc4cf971d8babe1a5dd310
Sokol Djala <sokol.djala@atis.al>
2020-01-20 16:02:48 +0100

    fix

--- lambda/postSurvey.js
+++ lambda/postSurvey.js
@@ -2,21 +2,27 @@ module.exports.handler = async function (event, context, callback) {
     const payload = JSON.parse(event.body)
   try {
     if (Array.isArray(payload) && payload.length === 11) {
-      payload.map((item)=> {
+      let error = false
+      payload.map((item) => {
         if(!item.id || item.id === "" || item.id === null || !item.value || item.value === "" || item.value === null){
           console.log("TCL: item", item.value)
+          error = true
+          return error
+        } 
+      })
+      if (error) {
+        return {
+          // return null to show no errors
+          statusCode: 422, // http status code
+          body: JSON.stringify("Error")
+        }
+        }else{
           return {
             // return null to show no errors
             statusCode: 422, // http status code
             body: JSON.stringify("Error")
           }
-        } 
-      })
-      return {
-        // return null to show no errors
-        statusCode: 200, // http status code
-        body: JSON.stringify("Success")
-      }
+        }
     }else{
       return {
         // return null to show no errors
