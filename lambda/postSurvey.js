ab33662e0f5f26afbbb3e2995bdfcc4b71ff6de3
Sokol Djala <sokol.djala@atis.al>
2020-01-20 15:31:40 +0100

    fix

--- lambda/postSurvey.js
+++ lambda/postSurvey.js
@@ -16,11 +16,12 @@ module.exports.handler = async function (event, context, callback) {
         statusCode: 200, // http status code
         body: JSON.stringify("Success")
       }
-    }
-    return {
-      // return null to show no errors
-      statusCode: 422, // http status code
-      body: JSON.stringify("Error")
+    }else{
+      return {
+        // return null to show no errors
+        statusCode: 422, // http status code
+        body: JSON.stringify("Error")
+      }
     }
   } catch (error) {
     return {
